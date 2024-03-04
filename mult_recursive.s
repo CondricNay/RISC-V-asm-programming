083731df856fdd01b1be15ffcb11cbe8d8d141a8
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2024-03-04 15:30:15 +0700

    add mult_recursive.s

--- mult_recursive.s
+++ mult_recursive.s
@@ -1,35 +1,47 @@
 .text
 main:
     # pass the first argument to a0
-    # pass the second argument
+    # pass the second argument to a1
     addi a0, x0, 110
     addi a1, x0, 50
     jal mult
-    
+
+    # print int
+    mv a1, a0   # by convention the reutnr value is always in a0
+    addi a0, x0, 1
+    ecall
+
     # exit cleanly
+    addi a0, x0, 10
+    ecall
 
 mult:
     # base case
-    # compaare a1 with 1 and with 1, if the two are equal you exit the mult function
-    
+    # compare a1 with 1, if the two are equal you exit the mult function
+    addi t0, x0, 1
+    beq a1, t0, exit_base_case
+
     # recursive case
     addi sp, sp, -4
-    sw ra, 0(sp) # storeing the value on to the stack
-    
-    
-    # a + mult(a, b-1)
+    sw ra, 0(sp) # storeing the ra value on to the stack
 
-    #save a0 and a1 on to the stack
-    
-    #pass the first argument to a0
-    # pass the second argument to a1
-
+    # mult(a, b-1)
+    addi sp, sp, -4
+    sw a0, 0(sp)
+    addi a1, a1, -1
     jal mult
-    
-    # by convention the result is in a0
-    # restrore a0 and a1 on to the stack
-    
-    # exit from the recursive case
-    add a0, a0, t0 # where t0 is result from mult(a, b-1)
+
+    # a + mult(a, b - 1)
+    mv t1 , a0
+
+    # restore the original "a" value before the call to mult
+    lw a0, 0(sp)
+    addi sp, sp, 4
+    add a0, a0, t1
+
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    jr ra
+
 exit_base_case:
     jr ra
